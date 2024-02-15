5450ed6aa4a2ffd71c92eb1f72a9acccb13a486d
RandDev <80163738+randwahdan@users.noreply.github.com>
2024-02-15 23:26:31 +0300

    Add project files.

---
+++ ShipShopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShipShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
